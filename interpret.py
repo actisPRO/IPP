1879257adc88a8a049271fe6ca5bb78bb05e44dd
Denis Karev <dkarev@protonmail.com>
2022-04-05 19:01:17 +0200

    Interpreter class

--- interpret.py
+++ interpret.py
@@ -1,15 +1,23 @@
 import argparse
+import sys
+
 from exit_code import ExitCode
+from interpreter import Interpreter
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('interpret.py', add_help=False)
-    parser.add_argument('--help', action='help', help='show this help message and exit')
-    parser.add_argument('--source', action='store', help='set an input XML file')
-    parser.add_argument('--input', action='store', help='set input for the interpretation')
+    parser.add_argument('--help', action='help', help='show this help message and exit.')
+    parser.add_argument('--source', action='store', help='set an input XML file.')
+    parser.add_argument('--input', action='store', help='set input for the interpretation.')
 
     args = parser.parse_args()
     if args.source is None and args.input is None:
-        print('At least one argument (--source or --input) must be set')
+        print('ERROR: at least one argument (--source or --input) must be set.', file=sys.stderr)
         exit(ExitCode.MISSING_ARGUMENT.value)
 
+    try:
+        interpreter = Interpreter(args.source, args.input)
+    except FileNotFoundError:
+        print('ERROR: specified file was not found.', file=sys.stderr)
+        exit(ExitCode.READ_ERROR.value)
---
+++ interpreter.py
@@ -0,0 +1,32 @@
+import io
+import os.path
+import sys
+import xml.etree.ElementTree as ET
+
+
+class Interpreter:
+    xml_tree = None
+    input_stream = None
+
+    @staticmethod
+    def __read_source_from_stdin():
+        result = ''
+        for line in sys.stdin:
+            result += line
+
+        return result
+
+    def __init__(self, source_file, input_file):
+        if source_file is None:
+            xml = self.__read_source_from_stdin()
+        else:
+            xml = source_file
+        self.xml_tree = ET.parse(xml)
+
+        if input_file is None:
+            self.input_stream = sys.stdin
+        else:
+            if not os.path.exists(input_file):
+                raise FileNotFoundError
+            else:
+                self.input_stream = io.open(input_file, 'r')
