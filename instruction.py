994a0f7aaaa125b148ec7d99181187ec872838df
Denis Karev <dkarev@protonmail.com>
2022-04-11 17:22:08 +0200

    6.4.1 instructions

--- instruction.py
+++ instruction.py
@@ -8,8 +8,44 @@ class Instruction:
         self.order = order
         self.args = []
 
-    def execute(self, ctx: Context):
-        print('exec')
-
     def add_arg(self, arg: Argument):
         self.args.append(arg)
+
+    def execute(self, ctx: Context):
+        print(self.opcode)
+
+        if self.opcode == 'MOVE':
+            self.move(ctx)
+        elif self.opcode == 'CREATEFRAME':
+            self.createframe(ctx)
+        elif self.opcode == 'PUSHFRAME':
+            self.pushframe(ctx)
+        elif self.opcode == 'POPFRAME':
+            self.popframe(ctx)
+        elif self.opcode == 'DEFVAR':
+            self.defvar(ctx)
+        elif self.opcode == 'CALL':
+            self.call(ctx)
+        elif self.opcode == 'RETURN':
+            self.exec_return(ctx)
+
+    def move(self, ctx: Context):
+        return
+
+    def createframe(self, ctx: Context):
+        return
+
+    def pushframe(self, ctx: Context):
+        return
+
+    def popframe(self, ctx: Context):
+        return
+
+    def defvar(self, ctx: Context):
+        return
+
+    def call(self, ctx: Context):
+        return
+
+    def exec_return(self, ctx: Context):
+        return
