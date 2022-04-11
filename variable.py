de0ca6063643bbd788452c9c18c71600c521e2f7
Denis Karev <dkarev@protonmail.com>
2022-04-11 19:52:29 +0200

    removed type check

--- variable.py
+++ variable.py
@@ -2,8 +2,3 @@ class Variable:
     def __init__(self, var_type, value):
         self.type = var_type
         self.value = value
-        self.check_type()
-
-    def check_type(self):
-        # todo: implement
-        return
