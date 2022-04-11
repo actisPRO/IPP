5df0f9883a0b132f2fe408fb49aa2f379d5565b0
Denis Karev <dkarev@protonmail.com>
2022-04-11 20:24:41 +0200

    sub

--- instruction.py
+++ instruction.py
@@ -163,6 +163,16 @@ class Instruction:
         return
 
     def sub(self, ctx: Context):
+        sym1 = ctx.get_variable_from_arg(self.args[1])
+        sym2 = ctx.get_variable_from_arg(self.args[2])
+        if sym1.type != 'int' or sym2.type != 'int':
+            ctx.error('SUB accepts only integer parameters.')
+            exit(ExitCode.BAD_OPERAND_TYPE)
+
+        result = int(sym1.value) - int(sym2.value)
+
+        var = self.args[0].value.split('@')
+        ctx.set_variable(var[0], var[1], 'int', result)
         return
 
     def mul(self, ctx: Context):
