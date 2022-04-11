597e387147e74e83cffe3f9ab12e5e489c11d107
Denis Karev <dkarev@protonmail.com>
2022-04-11 22:09:35 +0200

    jumpifeq

--- instruction.py
+++ instruction.py
@@ -461,6 +461,27 @@ class Instruction:
         return
 
     def jumpifeq(self, ctx: Context):
+        sym1 = ctx.get_variable_from_arg(self.args[1])
+        sym2 = ctx.get_variable_from_arg(self.args[2])
+
+        if sym1.type == 'nil' or sym2.type == 'nil':
+            var = self.args[0].value.split('@')
+            if sym1.type == 'nil' and sym2.type != 'nil' or sym1.type != 'nil' and sym2.type == 'nil':
+                return  # do nothing
+            else:
+                ctx.jump_to_label(self.args[0].value)
+            return
+
+        if sym1.type != sym2.type:
+            ctx.error('EQ only accepts parameters with equal types.')
+            exit(ExitCode.BAD_OPERAND_TYPE.value)
+
+        if sym1.type == 'int':
+            if int(sym1.value) == int(sym2.value):
+                ctx.jump_to_label(self.args[0].value)
+        else:
+            if sym1.value == sym2.value:
+                ctx.jump_to_label(self.args[0].value)
         return
 
     def jumpifneq(self, ctx: Context):
@@ -474,4 +495,3 @@ class Instruction:
 
     def exec_break(self, ctx: Context):
         return
-
